1b1f9d3359b535598c179d5179df65c5156375de
Sahanamurnal1108 <163160332+Sahanamurnal1108@users.noreply.github.com>
2024-03-12 22:33:20 +0530

    Add files via upload

---
+++ simple intrest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    float P;
+    float T;
+    float R;
+    float SI;
+    printf("enter the value of P,T,R");
+    scanf("%f%f%f%f",&P&T&R&S.I);
+    S.I=P*T*R/100;
+    printf("S.I=%f",S.I);
+}
