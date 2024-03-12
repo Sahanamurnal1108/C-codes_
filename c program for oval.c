62483da92b7e5b0fd2f98a7c091ce3c26cbf025b
Sahanamurnal1108 <163160332+Sahanamurnal1108@users.noreply.github.com>
2024-03-12 22:33:52 +0530

    Add files via upload

---
+++ c program for oval.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+    char ch;
+    printf("enter a char\n");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+    case 'a':printf("it is an vowel\n");
+        break;
+    case 'e':printf("it is an vowel\n");
+        break;
+    case 'i':printf("it is an vowel\n");
+        break;
+    case 'o':printf("it is an vowel\n");
+        break;
+    case 'u':printf("it is an vowel\n");
+        break;
+    case 'A':printf("it is an vowel\n");
+        break;
+    case 'E':printf("it is an vowel\n");
+        break;
+    case 'I':printf("it is an vowel\n");
+        break;
+    case 'O':printf("it is an vowel\n");
+        break;
+    case 'U':printf("it is an vowel\n");
+        break;
+        default:printf(" it is not an vowel\n");
+        break;
+    }
+}
